734ca810f087f119839ac186a5ed7088f9b49978
Serhii26 <25kontrakt@gmail.com>
2022-10-24 14:01:23 +0300

    commit4

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,4 @@
- import throttle from 'lodash.throttle';
+import throttle from 'lodash.throttle';
 
 import Player from '@vimeo/player';
 
@@ -6,26 +6,20 @@ const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const STORAGE_KEY = 'videoplayer-current-time';
 
-
-
-
 player.on('timeupdate', throttle(timeUpdate, 1000));
 
 function timeUpdate(event) {
   const time = event.seconds;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(time));
   // console.log(time);
-
-  
 }
 
-  const timeSave = localStorage.getItem(STORAGE_KEY);
+const timeSave = localStorage.getItem(STORAGE_KEY);
 
-  if (timeSave) {
-    // iframe.seconds = timeSave;
-    console.log(iframe.seconds);
-  }
-  
-player.setCurrentTime(timeSave);
+if (timeSave) {
+  // iframe.seconds = timeSave;
+  console.log(iframe.seconds);
+  player.setCurrentTime(timeSave);
+}
 
-
+// player.setCurrentTime(timeSave);
